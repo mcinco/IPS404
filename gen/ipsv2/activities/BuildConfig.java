40a46a96eddcd49663369d3815df5f52b00cce88
Micah Cinco <micahcinco@gmail.com>
2014-04-02 17:37:41 +1300

    Initial commit for new project

---
+++ gen/ipsv2/activities/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ipsv2.activities;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/ipsv2/objects/AccessPoint.java
@@ -0,0 +1,44 @@
+package ipsv2.objects;
+
+public class AccessPoint {
+
+	private String mac;
+	private String level;
+	private String x;
+	private String y;
+	private String decription;
+
+
+	public String getMac() {
+		return mac;
+	}
+	public void setMac(String mac) {
+		this.mac = mac;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	public String getX() {
+		return x;
+	}
+	public void setX(String x) {
+		this.x = x;
+	}
+	public String getY() {
+		return y;
+	}
+	public void setY(String y) {
+		this.y = y;
+	}
+	public String getDecription() {
+		return decription;
+	}
+	public void setDecription(String decription) {
+		this.decription = decription;
+	}
+
+
+}
